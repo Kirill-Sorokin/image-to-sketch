d8c6ce892e7c1ef792670257fb008bc118926ace
Kirill David Sorokin <96061027+Kirill-Sorokin@users.noreply.github.com>
2024-03-26 00:33:27 +0000

    updating and debugging all files.

--- combined_program/ImgDDLabel.py
+++ combined_program/ImgDDLabel.py
@@ -1,18 +1,18 @@
 from PyQt5 import QtCore, QtWidgets, QtGui, uic
 
 class ImgDDLabel(QtWidgets.QLabel):
-    def __init__(self, lineedit = None):
+    def __init__(self, lineedit=None):
         super().__init__()
         self.setAcceptDrops(True)
         self.lineedit = lineedit
         self.imagefile = None
         self.setAlignment(QtCore.Qt.AlignCenter)
         self.setText("Drag the Image here")
-        self.setStyleSheet('''background-color: rgb(255, 255, 255): 
+        self.setStyleSheet('''background-color: rgb(255, 255, 255); 
                            color: rgb(39, 86, 115);
-                           border-radius:10px;
+                           border-radius: 10px;
                            font: 75 9pt "MS Shell Dlg 2";''')
-    
+
     def dragEnterEvent(self, event):
         if event.mimeData().hasImage:
             event.accept()
@@ -24,7 +24,6 @@ class ImgDDLabel(QtWidgets.QLabel):
             event.setDropAction(QtCore.Qt.CopyAction)
             image_path = event.mimeData().urls()[0].toLocalFile()
             self.imagefile = image_path
-            self.set_image()
             self.set_image(image_path)
             event.accept()
         else:
@@ -33,22 +32,24 @@ class ImgDDLabel(QtWidgets.QLabel):
     def mouseDoubleClickEvent(self, event):
         print("Mouse Double Click Event Triggered")
         try:
-            fname = QtWidgets.QFileDialog.getopenFileName(self, 'Select Image to Sketch',
+            fname = QtWidgets.QFileDialog.getOpenFileName(self, 'Select Image to Sketch',
                                                           '.',"Image Files (*.png *.bmp *.jpg)")
-            if fname:
+            if fname[0]:  # Checking if the file path is not empty
                 self.imagefile = fname[0]
                 self.set_image(self.imagefile)
-        except Exception as ex:print(ex)
+        except Exception as ex:
+            print(ex)
 
     def set_image(self, image_path):
         try:
-            image_path = QtGui.QPixmap(image_path)
-            image_path = image_path.scaled(QtCore.QSize(self.width(),
-                                                        self.height()),
-                                                        QtCore.Qt.KeepAspectRatio,
-                                                        QtCore.Qt.SmoothTransformation)
-            self.setPixmap(image_path)
-        except Exception as ex:print(ex)
+            image = QtGui.QPixmap(image_path)
+            image = image.scaled(QtCore.QSize(self.width(),
+                                              self.height()),
+                                 QtCore.Qt.KeepAspectRatio,
+                                 QtCore.Qt.SmoothTransformation)
+            self.setPixmap(image)
+        except Exception as ex:
+            print(ex)
 
 if __name__ == "__main__":
     import sys
@@ -57,8 +58,9 @@ if __name__ == "__main__":
         app = QtWidgets.QApplication(sys.argv)
         ui = uic.loadUi('window.ui')
         imgddlabel = ImgDDLabel()
-        ui.l1.addWidget(imgddlabel)
+        ui.l1.addWidget(imgddlabel)  # Ensure this matches your actual layout in 'window.ui'
         ui.show()
         app.exec_()
-    except Exception as ex:print(ex)
-    print('running finished')             
+    except Exception as ex:
+        print(ex)
+    print('running finished')
--- combined_program/sketcher.py
+++ combined_program/sketcher.py
@@ -1,4 +1,3 @@
-# python3 -m pip install opencv-python opencv-python-headless
 import cv2, numpy, time, traceback
 
 def webcam():
@@ -7,57 +6,55 @@ def webcam():
     while True:
         ret, frame = video_capture.read(0)
         cv2.putText(frame, "Image-to-Sketch by: {}".format(stname), (430, 30),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0,0,255), 1)
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 0, 255), 1)
         cv2.imshow("Webcam", frame)
-        if cv2.waitKey(1)==ord('q'):
+        if cv2.waitKey(1) == ord('q'):
             break
-        elif cv2.waitKey(1)==ord('c'):
+        elif cv2.waitKey(1) == ord('c'):
             print('Sketching')
             print(type(frame))
             Sketcher(frame=frame)
     video_capture.release()
     # cv2.destroyAllWindows()
 
-def Sketcher(img = None, frame = None):
+def Sketcher(img=None, frame=None):
     try:
         if img:
             print('Sketching from img')
             image = cv2.imread(img)
-            grey_img = cv2.cvtColor(image, cv2.COLOR_BAYER_BG2GRAY)
+            # Correcting the typo in the color conversion
+            grey_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
             invert = cv2.bitwise_not(grey_img)
             blur = cv2.GaussianBlur(invert, (61, 61), 0)
             invertedblur = cv2.bitwise_not(blur)
-            sketch = cv2.divide(grey_img, invertedblur, scale = 256.0)
+            sketch = cv2.divide(grey_img, invertedblur, scale=256.0)
             cv2.imshow("Sketch", sketch)
             while True:
-                # Press 'q' to 'Quit'.
                 if cv2.waitKey(1) == ord('q'):
                     cv2.destroyAllWindows()
                     break
-                # Press 's' to 'Save'.
                 elif cv2.waitKey(1) == ord('s'):
-                    img = './Sketches/'+'sketch-'+time.strftime('%d-%m-%y-%H-%M-%S')+'.png'
-                    cv2.imwrite(img, sketch)
-                    print('Image Saved as: ', img)
+                    img_path = './Sketches/'+'sketch-'+time.strftime('%d-%m-%y-%H-%M-%S')+'.png'
+                    cv2.imwrite(img_path, sketch)
+                    print('Image Saved as: ', img_path)
                     cv2.destroyAllWindows()
                     break
         elif type(frame) == numpy.ndarray:
             print('Sketching from Webcam')
-            image = frame
-            grey_img = cv2.bitwise_not(grey_img)
+            grey_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # Correctly applying color conversion
             invert = cv2.bitwise_not(grey_img)
             blur = cv2.GaussianBlur(invert, (61, 61), 0)
             invertedblur = cv2.bitwise_not(blur)
-            sketch = cv2.divide(grey_img, invertedblur, scale = 256.0)
+            sketch = cv2.divide(grey_img, invertedblur, scale=256.0)
             cv2.imshow("Sketch", sketch)
             while True:
                 if cv2.waitKey(1) == ord('q'):
                     cv2.destroyAllWindows()
                     break
                 elif cv2.waitKey(1) == ord('s'):
-                    img = './Sketches/'+'sketch-'+time.strftime('%d-%m-%y-%H-%M-%S')+'.png'
-                    cv2.imwrite(img,sketch)
-                    print('Image Saved as: ',img)
+                    img_path = './Sketches/'+'sketch-'+time.strftime('%d-%m-%y-%H-%M-%S')+'.png'
+                    cv2.imwrite(img_path, sketch)
+                    print('Image Saved as: ', img_path)
                     cv2.destroyAllWindows()
                     break
         else:
@@ -68,4 +65,3 @@ def Sketcher(img = None, frame = None):
 if __name__ == "__main__":
     print()
     webcam()
-
